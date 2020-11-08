d51f6d8bdcb65d49e28d2751872c8c861e395b5d
mohammed183 <mohammedsaeed183@hotmail.com>
2020-11-08 23:03:59 +0300

    Create Task01.c

---
+++ Task01/Task01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float d;
+    printf("Please Enter the amount in USD: ");
+    scanf("%f",&d);
+    printf("Amount in EGP: %f\n",d*15.69);
+    return 0;
+}
